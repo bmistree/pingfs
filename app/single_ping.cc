3c936cba64cdecb5703706ca369074c4bc84e672
Behram Mistree <bmistree@gmail.com>
2017-08-12 23:09:22 -0700

    Use program options for test app

--- app/single_ping.cc
+++ app/single_ping.cc
@@ -2,16 +2,52 @@
 
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/program_options.hpp>
 #include <pingfs/ping/ping.hpp>
 
+/**
+ * Returns true if command line parsing passed; false otherwise.
+ */
+bool parse_command_line(int argc, char** argv, 
+    std::string& hostname, std::string& ping_content) {
+    boost::program_options::options_description desc("Options");
+    desc.add_options()
+        ("help", "Print help messages")
+        ("hostname",
+            boost::program_options::value<std::string>(&hostname)->required(),
+            "Name of host to ping")
+        ("content",
+            boost::program_options::value<std::string>(&ping_content)->required(),
+            "Content to put in ping body");
+
+    boost::program_options::variables_map vm;
+    boost::program_options::store(
+        boost::program_options::parse_command_line(argc, argv, desc),
+        vm);
+
+    // The user specified the help flag
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return false;
+    }
+    // Throw exceptions for missing arguments
+    try {
+        boost::program_options::notify(vm);
+    } catch(std::exception ex) {
+        std::cerr << "Error: " << ex.what() << "\n";
+        return false;
+    }
+    return true;
+}
+
+
 int main(int argc, char** argv) {
-    if (argc < 3) {
-        std::cerr<<"\nUsage: ./single_ping [HOST] [BODY CONTENT]\n\n";
+    std::string hostname;
+    std::string ping_content;
+
+    if (!parse_command_line(argc, argv, hostname, ping_content)) {
         return 1;
     }
-    std::string hostname(argv[1]);
-    std::string ping_content(argv[2]);
-
     boost::asio::io_service io_service;
     pingfs::ping(ping_content, hostname, io_service);
     io_service.run();
