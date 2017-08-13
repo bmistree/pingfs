8ed2f41f8906724f69feafcb901747724b472aa0
Behram Mistree <bmistree@gmail.com>
2017-08-13 10:54:13 -0700

    Fix checksum calculation for echo requests

--- lib/src/ping/echo_request.cc
+++ lib/src/ping/echo_request.cc
@@ -4,13 +4,17 @@ namespace pingfs {
 
 uint16_t EchoRequest::checksum(uint16_t identifier, 
     uint16_t sequence_number, const std::string& body) {
-    uint32_t sum = (TYPE << 8) + CODE + htons(identifier)
-        + htons(sequence_number);
+    // Note that we do *not* need htons here because we're doing a
+    // mathematical operation on the data, whose value shouldn't
+    // depend on endianness.
+    uint32_t sum = (TYPE << 8) + CODE + identifier + sequence_number;
     for (unsigned int i = 0; i < body.size(); ++i) {
         uint8_t val = static_cast<uint8_t>(body[i]);
         sum += ((i % 2) == 0) ? (val << 8) : val;
     }
     sum += (sum >> 16);
+    // Note that we do need ntohs here because, by definition, 
+    // the value that we generate will be in network order.
     return ntohs(static_cast<uint16_t>(~sum));
 }
 
@@ -34,8 +38,8 @@ std::ostream& operator<< (std::ostream& os, const EchoRequest& request) {
     os.put(static_cast<char>(EchoRequest::TYPE));
     os.put(static_cast<char>(EchoRequest::CODE));
     put_short(os, htons(request.checksum_));
-    put_short(os, htons(request.identifier_));
-    put_short(os, htons(request.sequence_number_));
+    put_short(os, request.identifier_);
+    put_short(os, request.sequence_number_);
     os << request.body_;
     return os;
 }
