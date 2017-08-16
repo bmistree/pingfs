2ecc72151336b386d8a9e6d5f77083b19eb0a487
Behram Mistree <bmistree@gmail.com>
2017-08-15 21:16:52 -0700

    Added script to call linter

---
+++ scripts/codecheck.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Relative to root of project
+LIB_DIR=$DIR/../lib
+APP_DIR=$DIR/../app
+
+ALL_FILES=($(find $LIB_DIR -name *.cc))
+ALL_FILES+=($(find $LIB_DIR -name *.hpp))
+ALL_FILES+=($(find $APP_DIR -name *.cc))
+ALL_FILES+=($(find $APP_DIR -name *.hpp))
+ALL_FILES+=($(find $TEST_DIR -name *.hpp))
+ALL_FILES+=($(find $TEST_DIR -name *.cc))
+
+python $DIR/../deps/cpplint/cpplint.py --filter -legal/copyright ${ALL_FILES[@]}
+                                                                                      
