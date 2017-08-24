0b38c6c87af708ea4db4b03d59c5933c895cb50d
Behram Mistree <bmistree@gmail.com>
2017-08-23 23:40:48 -0700

    Update AsyncBlockManager to handle updated blocks

--- lib/include/pingfs/block/block_manager/async_block_manager.hpp
+++ lib/include/pingfs/block/block_manager/async_block_manager.hpp
@@ -1,24 +1,45 @@
 #ifndef _ASYNC_BLOCK_MANAGER_
 #define _ASYNC_BLOCK_MANAGER_
 
+#include <boost/thread/mutex.hpp>
+
 #include <pingfs/block/block_data/block_data.hpp>
 #include <pingfs/block/block_request.hpp>
 #include <pingfs/block/block_response.hpp>
 #include <pingfs/block/block_manager/block_manager.hpp>
+#include <pingfs/util/subscriber.hpp>
 
 #include <unordered_map>
+#include <vector>
 
+#include "async_response.hpp"
 #include "block_manager.hpp"
 
 namespace pingfs {
 
-class AsyncBlockManager : public BlockManager {
+class AsyncBlockManager :
+        public BlockManager,
+        public Subscriber<std::shared_ptr<const Block>> {
  public:
     AsyncBlockManager();
     virtual ~AsyncBlockManager();
 
  public:
+    // BlockManager overrides
     const BlockResponse get_blocks(const BlockRequest& block_request) override;
+
+ public:
+    // Subscriber overrides
+    void process(const std::shared_ptr<const Block>& block) override;
+
+ private:
+    boost::mutex map_mutex_;
+    /**
+     * A map from block ids to those responses that are waiting
+     * on those blocks.
+     */
+    std::unordered_map<BlockId, std::vector<std::shared_ptr<AsyncResponse>>>
+        waiting_;
 };
 
 }  // namespace pingfs
--- lib/src/block/block_manager/async_block_manager.cc
+++ lib/src/block/block_manager/async_block_manager.cc
@@ -2,16 +2,51 @@
 
 namespace pingfs {
 
-AsyncBlockManager::AsyncBlockManager() {
+AsyncBlockManager::AsyncBlockManager()
+  : map_mutex_(),
+    waiting_() {
 }
 
 AsyncBlockManager::~AsyncBlockManager() {
 }
 
+void AsyncBlockManager::process(const std::shared_ptr<const Block>& block) {
+    boost::mutex::scoped_lock scope(map_mutex_);
+    if (waiting_.find(block->get_block_id()) == waiting_.end()) {
+        return;
+    }
+
+    std::vector<std::shared_ptr<AsyncResponse>>& waiting_vec =
+        waiting_[block->get_block_id()];
+
+    for (auto iter = waiting_vec.cbegin();
+         iter != waiting_vec.cend(); ++iter) {
+        (*iter)->update(block);
+    }
+
+    waiting_.erase(block->get_block_id());
+}
+
 const BlockResponse AsyncBlockManager::get_blocks(
     const BlockRequest& block_request) {
-    // FIXME: Must finish
-    throw "Unsupported operation";
+
+    const std::vector<BlockId>& blocks = block_request.get_blocks();
+    std::shared_ptr<AsyncResponse> resp_ptr =
+        std::make_shared<AsyncResponse>(block_request);
+
+    {
+        boost::mutex::scoped_lock scope(map_mutex_);
+        for (auto block_iter = blocks.cbegin();
+             block_iter != blocks.cend(); ++block_iter) {
+            std::vector<std::shared_ptr<AsyncResponse>>& resp_vec =
+                waiting_[*block_iter];
+            resp_vec.push_back(resp_ptr);
+        }
+    }
+
+    // FIXME: consider changing to return the shared pointer
+    // directly.
+    return *(resp_ptr->get_future().get());
 }
 
 }  // namespace pingfs
